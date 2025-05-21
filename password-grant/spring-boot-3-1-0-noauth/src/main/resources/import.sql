0e42c7ef74e8bc4ef7d9f4d956f68d25619c75bd
Matheus Farias <matheusaf.contato@gmail.com>
2025-05-21 15:44:01 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$t1Fmb6Hc9.S0cJ6smrPGtex8zteKskUk1m.GEKL1a6PlVOTWQ1kw6');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$t1Fmb6Hc9.S0cJ6smrPGtex8zteKskUk1m.GEKL1a6PlVOTWQ1kw6');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
